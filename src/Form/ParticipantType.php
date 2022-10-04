d47ccf0140a98a9cf78f386cff6c7955d96f2b86
Francorce <69760008+francorce@users.noreply.github.com>
2022-10-04 17:12:41 +0200

    participant type

--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Participant;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,15 +13,25 @@ class ParticipantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('password')
+//            ->add('password')
             ->add('email')
-            ->add('roles')
+            //->add('roles')
             ->add('nom')
             ->add('pseudo')
             ->add('prenom')
             ->add('telephone')
-            ->add('admin')
-            ->add('actif')
+//            ->add('admin')
+//            ->add('actif')
+            ->add('photo')
+            ->add('estRattache', EntityType::class,[
+                'class' => 'App\Entity\Site',
+                'label' => ' ',
+                'choice_label' => function($ville) {
+                    return $ville->getNomSite();
+                },
+                'multiple' => false,
+                'expanded' => false
+            ])
         ;
     }
 
