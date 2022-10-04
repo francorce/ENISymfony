51e6aa424a0c5cedd9514953e79e2b8d07f3aa11
Francorce <69760008+francorce@users.noreply.github.com>
2022-10-04 10:45:34 +0200

    Ville et Sites pour admin

--- src/Controller/Admin/SiteController.php
+++ src/Controller/Admin/SiteController.php
@@ -2,17 +2,81 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Site;
+use App\Form\SiteType;
+use App\Repository\SitesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class SiteController extends AbstractController
 {
     #[Route('/admin/site', name: 'app_admin_site')]
-    public function index(): Response
+    public function index(SitesRepository $sitesRepository): Response
     {
+        $sites = $sitesRepository->findAll();
         return $this->render('admin/site/site.html.twig', [
+            "sites" => $sites,
+        ]);
+    }
+
+    #[Route('/admin/site/supprimer/{id}', name: 'app_admin_site_supprimer', requirements: ['id' => '\d+'])]
+    public function supprimer(SitesRepository $sitesRepository,  Request $request, EntityManagerInterface $entityManager, $id = null): Response
+    {
+        $site = $sitesRepository->find($id);
+        if ($site) {
+            $entityManager->remove($site);
+            $entityManager->flush();
+            $this->addFlash('success', 'Le site a bien été supprimé');
+        } else {
+            $this->addFlash('danger', 'La site n\'existe pas');
+        }
+        return $this->redirectToRoute('app_admin_site');
+    }
+
+    #[Route('/admin/site/ajouter', name: 'app_admin_site_ajouter')]
+    #[Route('/admin/site/modifier/{id}', name: 'app_admin_site_modifier', requirements: ['id' => '\d+'])]
+    public function editer(UserPasswordHasherInterface $hasher,SluggerInterface $slugger,SitesRepository $sitesRepository,  Request $request, EntityManagerInterface $entityManager, $id = null): Response
+    {
+
+        if ($request->attributes->get('_route') == 'app_admin_site_ajouter') {
+            $site = new Site();
+
+        } else {
+            $site = $sitesRepository->find($id);
+            //$bien = $entityManager->getRepository(Bien::class)->find($request->attributes->get('id'));
+        }
+
+        $form = $this->createForm(SiteType::class, $site);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $site = $form->getData();
+
+
+            $entityManager->persist($site);
+            $entityManager->flush();
+            if ($request->attributes->get('_route') == 'app_admin_site_ajouter') {
+                $this->addFlash(
+                    'success',
+                    'Site ajouté avec succès'
+                );
+            } else {
+                $this->addFlash(
+                    'success',
+                    'Site modifié avec succès'
+                );
+            }
+            return $this->redirectToRoute('app_admin_site');
+        }
+        return $this->render('admin/site/editerSite.html.twig', [
             'controller_name' => 'SiteController',
+            'formulaireSite' => $form->createView(),
         ]);
     }
 }
--- src/Controller/Admin/VilleController.php
+++ src/Controller/Admin/VilleController.php
@@ -2,17 +2,82 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Ville;
+use App\Form\VilleType;
+use App\Repository\VillesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class VilleController extends AbstractController
 {
     #[Route('/admin/ville', name: 'app_admin_ville')]
-    public function index(): Response
+    public function index(VillesRepository $villesRepository): Response
     {
+        $villes = $villesRepository->findAll();
         return $this->render('admin/ville/ville.html.twig', [
+            "villes" => $villes,
+        ]);
+    }
+
+
+    #[Route('/admin/ville/supprimer/{id}', name: 'app_admin_ville_supprimer', requirements: ['id' => '\d+'])]
+    public function supprimer(VillesRepository $villesRepository,  Request $request, EntityManagerInterface $entityManager, $id = null): Response
+    {
+        $ville = $villesRepository->find($id);
+        if ($ville) {
+            $entityManager->remove($ville);
+            $entityManager->flush();
+            $this->addFlash('success', 'La ville a bien été supprimé');
+        } else {
+            $this->addFlash('danger', 'La ville n\'existe pas');
+        }
+        return $this->redirectToRoute('app_admin_ville');
+    }
+
+    #[Route('/admin/ville/ajouter', name: 'app_admin_ville_ajouter')]
+    #[Route('/admin/ville/modifier/{id}', name: 'app_admin_ville_modifier', requirements: ['id' => '\d+'])]
+    public function editer(UserPasswordHasherInterface $hasher,SluggerInterface $slugger,VillesRepository $villesRepository,  Request $request, EntityManagerInterface $entityManager, $id = null): Response
+    {
+
+        if ($request->attributes->get('_route') == 'app_admin_ville_ajouter') {
+            $ville = new Ville();
+
+        } else {
+            $ville = $villesRepository->find($id);
+            //$bien = $entityManager->getRepository(Bien::class)->find($request->attributes->get('id'));
+        }
+
+        $form = $this->createForm(VilleType::class, $ville);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ville = $form->getData();
+
+
+            $entityManager->persist($ville);
+            $entityManager->flush();
+            if ($request->attributes->get('_route') == 'app_admin_ville_ajouter') {
+                $this->addFlash(
+                    'success',
+                    'Ville ajouté avec succès'
+                );
+            } else {
+                $this->addFlash(
+                    'success',
+                    'Ville modifié avec succès'
+                );
+            }
+            return $this->redirectToRoute('app_admin_ville');
+        }
+        return $this->render('admin/ville/editerVille.html.twig', [
             'controller_name' => 'VilleController',
+            'formulaireVille' => $form->createView(),
         ]);
     }
 }
