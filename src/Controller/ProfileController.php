95affacaa55233a32286a60ccbafa6cf74c94ce2
Francorce <69760008+francorce@users.noreply.github.com>
2022-10-10 11:05:59 +0200

    fix photo ProfilModifier

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -7,10 +7,12 @@ use App\Form\ParticipantType;
 use App\Repository\ParticipantRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProfileController extends AbstractController
 {
@@ -29,13 +31,35 @@ class ProfileController extends AbstractController
     }
 
     #[Route('/profileModifier', name: 'app_profile_modifier')]
-    public function modif(UserPasswordHasherInterface $hasher, EntityManagerInterface $entityManager, Request $request): Response
+    public function modif(UserPasswordHasherInterface $hasher, EntityManagerInterface $entityManager, Request $request, SluggerInterface $slugger): Response
     {
         $participants = $this->getUser();
         $form = $this->createForm(ParticipantType::class, $participants);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $photoFile = $form->get('photo')->getData();
+            if ($photoFile) {
+                $originalFilename = pathinfo($photoFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $photoFile->guessExtension();
+
+                try {
+                    $photoFile->move(
+                        $this->getParameter('photo_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+                $participants->setPhoto($newFilename);
+            }
+
             $profile = $form->getData();
+
+            $photoFile = $form->get('photo')->getData();
+
+
 
             if (!$this->isGranted('ROLE_ADMIN')) {
                 $participants->setRoles(['ROLE_USER']);
